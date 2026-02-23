3690c65d35fc46862e1f8d0b2d6c4ad235c87cd3
girindra <gdwiixa@gmail.com>
2026-02-23 17:35:25 +0700

    feat: implement controllers

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthLoginRequest;
+use App\Http\Requests\AuthRegisterRequest;
+use App\Http\Resources\AuthLoginResource;
+use App\Http\Resources\AuthRegisterResource;
+use App\Repositories\Contracts\AuthRepositoryInterface;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    private AuthRepositoryInterface $authRepository;
+
+    public function __construct(AuthRepositoryInterface $authRepository)
+    {
+        $this->authRepository = $authRepository;
+    }
+
+    public function register(AuthRegisterRequest $request)
+    {
+        $user = $this->authRepository->register($request->validated());
+        
+        return response()->json([
+            'message' => 'Register success',
+            'data' => new AuthRegisterResource($user),
+        ], 201);
+    }
+
+    public function login(AuthLoginRequest $request)
+    {
+        $user = $this->authRepository->login($request->validated());
+
+        return response()->json([
+            'message' => 'Login success',
+            'data' => new AuthLoginResource($user)
+        ]);
+    }
+
+    public function logout()
+    {
+        $this->authRepository->logout();
+        
+        return response()->json([
+            'message' => 'logout success',
+        ], 200);
+    }
+}
